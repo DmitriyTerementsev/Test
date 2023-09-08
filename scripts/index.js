b5651851ec7b9e8942ddb777bab31bd90f028487
Dmitriy Terementsev <d.terementsev@yandex.ru>
2023-09-08 23:34:29 +0300

    new function

--- scripts/index.js
+++ scripts/index.js
@@ -2,6 +2,28 @@ const main = document.querySelector(".main");
 const buttonTemplate = document.querySelector("#button-template");
 const img = document.querySelector('.image')
 
+function toggleBtn(obj, btn){
+  if (obj.classList.contains('button_active')){
+    obj.classList.remove('button_active')
+    obj.querySelector('.button__text').classList.remove('button__text_active')
+    obj.querySelector('.button__vector').src = "./images/Vector_11.svg"
+    document.removeEventListener("keydown", handleCloseByClick)
+  } else {
+    obj.classList.add('button_active')
+    document.addEventListener("keydown", handleCloseByClick)
+    obj.querySelector('.button__text').classList.add('button__text_active')
+    obj.classList.contains("button_active")
+        ? (btn.src = "./images/Vector11.svg")
+        : (btn.src = "./images/Vector_11.svg");
+  }
+  }
+  
+  function handleCloseByClick(evt) {
+    if (evt.key === "Escape") {
+      toggleBtn(document.querySelector(".button_active"));
+    }
+  }
+
 function getButtonElement(buttons) {
   const buttonElement = buttonTemplate.content.cloneNode(true);
   const button = buttonElement.querySelector(".button");
@@ -12,16 +34,11 @@ function getButtonElement(buttons) {
   button.style.border = buttons.color;
   button.style.top = buttons.positionY;
   button.style.left = buttons.positionX;
-  
   button.addEventListener("click", () => {
-    button.classList.toggle("button_active");
-    buttonText.classList.toggle("button__text_active");
-    button.classList.contains("button_active")
-      ? (buttonVector.src = "./images/Vector11.svg")
-      : (buttonVector.src = "./images/Vector_11.svg");
-    
-  });
-
+    toggleBtn(button, buttonVector)
+    handleCloseByClick()
+  })
+  
   return buttonElement;
 }
 
